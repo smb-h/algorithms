5458dfa6312426de23ae0b12e68a6fdee2cfafa9
SMB H <smbhop@pm.me>
2023-05-12 21:23:22 +0330

    :bulb: update(src): Updating project content

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='algorithms',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementation of popular algorithms',
+    author='SMB H',
+    license='MIT',
+)
